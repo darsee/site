3c78f3a00b9cbc08bd641d10e26c2b3244b6640a
Seamus D'Arcy <darsee@gmail.com>
2017-07-21 16:24:54 +0100

    Remove dark mode, update css

--- assets/stuff.js
+++ assets/stuff.js
@@ -6,25 +6,4 @@ if (randomEmoji) {
   randomEmoji.textContent = emoji[Math.floor(Math.random() * emoji.length)]
 }
 
-const supportsLocalStorage = 'localStorage' in window
-const darkModeButton = document.querySelector(".js-toggle-dark-mode")
-
-if (darkModeButton) {
-  darkModeButton.addEventListener('click', switchMode)
-}
-
-if (supportsLocalStorage) {
-  const darkModeOn = localStorage.getItem('darkMode')
-  if (darkModeOn) switchMode()
-}
-
-function switchMode () {
-  const on = document.body.classList.toggle('dark-mode')
-  if (darkModeButton) darkModeButton.setAttribute('aria-pressed', on)
-
-  if (supportsLocalStorage) {
-    on ? localStorage.setItem('darkMode', true) : localStorage.removeItem('darkMode')
-  }
-}
-
 _gaq.push(['_trackPageview'])
